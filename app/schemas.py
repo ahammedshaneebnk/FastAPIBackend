c9699dc1d9e0958bd78b2b822c5bb0d0d307b360
Ahammed Shaneeb N K <22722617+ahammedshaneebnk@users.noreply.github.com>
2026-01-17 14:25:15 +0530

    field desc in pydantic

--- app/schemas.py
+++ app/schemas.py
@@ -5,8 +5,15 @@ def generate_random_destination() -> int:
     return randint(60000, 699999)
 
 class Shipment(BaseModel):
-    content : str = Field(max_length=100)
-    weight : float = Field(ge=1, le=25)
+    content : str = Field(
+        max_length=100,
+        description="Description of the shipment content"
+    )
+    weight : float = Field(
+        description="Weight of the shipment in kilograms",
+        ge=1,
+        le=25)
     destination : int | None = Field(
+        description="Destination postal code",
         default_factory=generate_random_destination
     )
